a7cd361d292c067a87bf7e42df0f148896c68005
Hakeem Mohammad Sallauddin <sallauddinab@gmail.com>
2026-07-25 00:29:06 +0530

    Use environment variable for backend API URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL + "/api/v1",
 });
 
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
@@ -13,7 +12,6 @@ api.interceptors.request.use((config) => {
   }
 
   return config;
-
 });
 
 export default api;
